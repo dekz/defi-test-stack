917b7e90c6609c43420c37a50267249a32e1a238
Jacob Evans <jacob@dekz.net>
2018-11-29 15:41:11 +1100

    Remove 0x artifacts and use @0x/contract-artifacts

--- src/migrations/zeroEx/artifacts.ts
+++ src/migrations/zeroEx/artifacts.ts
@@ -1,15 +1,10 @@
+import * as zeroExArtifacts from '@0x/contract-artifacts';
 import { ContractArtifact } from 'ethereum-types';
 
-import * as ERC20Proxy from '../../../artifacts/zeroEx/ERC20Proxy.json';
-import * as ERC721Proxy from '../../../artifacts/zeroEx/ERC721Proxy.json';
-import * as Exchange from '../../../artifacts/zeroEx/Exchange.json';
-import * as Forwarder from '../../../artifacts/zeroEx/Forwarder.json';
-import * as OrderValidator from '../../../artifacts/zeroEx/OrderValidator.json';
-
 export const artifacts = {
-    ERC20Proxy: (ERC20Proxy as any) as ContractArtifact,
-    ERC721Proxy: (ERC721Proxy as any) as ContractArtifact,
-    Exchange: (Exchange as any) as ContractArtifact,
-    Forwarder: (Forwarder as any) as ContractArtifact,
-    OrderValidator: (OrderValidator as any) as ContractArtifact,
+    ERC20Proxy: (zeroExArtifacts.ERC20Proxy as any) as ContractArtifact,
+    ERC721Proxy: (zeroExArtifacts.ERC721Proxy as any) as ContractArtifact,
+    Exchange: (zeroExArtifacts.Exchange as any) as ContractArtifact,
+    Forwarder: (zeroExArtifacts.Forwarder as any) as ContractArtifact,
+    OrderValidator: (zeroExArtifacts.OrderValidator as any) as ContractArtifact,
 };
